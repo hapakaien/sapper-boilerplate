8c64b9ed6d54ac79bf7dc2b99dced5790d1a0bb7
Ahmad Husen <hapakaien@gmail.com>
2021-01-09 13:24:46 +0700

    feat: change base path as default

--- src/server.ts
+++ src/server.ts
@@ -6,10 +6,8 @@ import * as sapper from '@sapper/server';
 const { PORT, NODE_ENV } = process.env;
 const dev = NODE_ENV === 'development';
 
-const url = dev ? '/' : process.env.APP_PATH;
-
 polka() // You can also use Express
-	.use(url, compression({ threshold: 0 }), sirv('static', { dev }), sapper.middleware())
+	.use(compression({ threshold: 0 }), sirv('static', { dev }), sapper.middleware())
 	.listen(PORT, (err) => {
 		if (err) console.log('error', err);
 	});
